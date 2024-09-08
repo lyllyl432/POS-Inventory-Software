316fe798140a62dbb45a3b3df64cb2e03e776f5c
lyl <johnlyl@geniihut.com>
2024-09-08 23:06:16 +0800

    refactor view layout

--- app/Controllers/Products.php
+++ app/Controllers/Products.php
@@ -6,8 +6,6 @@ class Products extends BaseController
 {
     public function index(): string
     {
-        return view('templates/header')
-            . view('pages/create-product')
-            . view('templates/footer');
+        return view('pages/product/create-product');
     }
 }
--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,3 +1,4 @@
+<h1>Hello world</h1>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="<?= base_url() ?>js/app.js"></script>
 </body>
